d795b1bba7b39a368ac971743beaf084019b9ed9
Adam Rodger <adam.rodger@gmail.com>
2017-12-04 13:07:10 +0000

    Day 4

---
+++ tests/AdventOfCode.Tests/Day04Tests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day04Tests
+    {
+        [Fact]
+        public void Part1_WhenCalled_ReturnsCorrectCount()
+        {
+            const int expected = 386;
+
+            int actual = Day04.Part1();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Part2_WhenCalled_ReturnsCorrectCount()
+        {
+            const int expected = 208;
+
+            int actual = Day04.Part2();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
