2ed01532038e202854f609ea890b1ffe961170c6
Adam Rodger <adam.rodger@gmail.com>
2017-12-08 08:13:35 +0000

    Day 8

---
+++ src/AdventOfCode/Day08.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 8
+    /// </summary>
+    public class Day08
+    {
+        /// <summary>
+        /// Solve for the input file
+        /// </summary>
+        /// <returns>Highest at the end, highest ever</returns>
+        public (int, int) Solve()
+        {
+            string[] lines = File.ReadAllLines("inputs/day08.txt");
+            return this.Solve(lines);
+        }
+
+        /// <summary>
+        /// Solve for the given input
+        /// </summary>
+        /// <returns>Highest at the end, highest ever</returns>
+        public (int, int) Solve(ICollection<string> input)
+        {
+            var registers = new Dictionary<string, int>();
+            int max = 0;
+
+            foreach (string line in input)
+            {
+                var instruction = Instruction.Parse(line);
+                if (instruction.Condition(registers))
+                {
+                    instruction.Action(registers);
+                    max = Math.Max(max, registers.Values.Max());
+                }
+            }
+
+            return (registers.Values.Max(), max);
+        }
+    }
+
+    /// <summary>
+    /// Instruction
+    /// </summary>
+    public class Instruction
+    {
+        private static readonly Regex ParseRegex = new Regex(@"(\w+) (inc|dec) (-?\d+) if (\w+) ([!<>!=]+) (-?\d+)", RegexOptions.Compiled);
+
+        /// <summary>
+        /// Action to perform if the condition is met
+        /// </summary>
+        public Action<IDictionary<string, int>> Action { get; private set; }
+
+        /// <summary>
+        /// Condition of the action
+        /// </summary>
+        public Predicate<IDictionary<string, int>> Condition { get; private set; }
+
+        /// <summary>
+        /// Parse an instruction from the input string
+        /// </summary>
+        /// <param name="input">Input string</param>
+        /// <returns>Parsed instruction</returns>
+        public static Instruction Parse(string input)
+        {
+            Match match = ParseRegex.Match(input);
+
+            string target = match.Groups[1].Value;
+            bool increment = match.Groups[2].Value.Equals("inc");
+            int step = int.Parse(match.Groups[3].Value);
+
+            if (!increment)
+            {
+                step *= -1;
+            }
+
+            Action<IDictionary<string, int>> action = registers =>
+            {
+                registers.TryGetValue(target, out int value);
+                registers[target] = value + step;
+            };
+
+            string conditionRegister = match.Groups[4].Value;
+            string conditionOperation = match.Groups[5].Value;
+            int conditionValue = int.Parse(match.Groups[6].Value);
+
+            Predicate<int> condition;
+
+            switch (conditionOperation)
+            {
+                case "<":
+                    condition = x => x < conditionValue;
+                    break;
+                case ">":
+                    condition = x => x > conditionValue;
+                    break;
+                case "<=":
+                    condition = x => x <= conditionValue;
+                    break;
+                case ">=":
+                    condition = x => x >= conditionValue;
+                    break;
+                case "==":
+                    condition = x => x == conditionValue;
+                    break;
+                case "!=":
+                    condition = x => x != conditionValue;
+                    break;
+                default:
+                    throw new InvalidOperationException($"Unknown condition: {conditionOperation}");
+            }
+
+            Predicate<IDictionary<string, int>> predicate = registers =>
+            {
+                registers.TryGetValue(conditionRegister, out int value);
+                return condition(value);
+            };
+
+            return new Instruction
+            {
+                Action = action,
+                Condition = predicate
+            };
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day08Tests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day08Tests
+    {
+        private static readonly string[] Input =
+        {
+            "b inc 5 if a > 1",
+            "a inc 1 if b < 5",
+            "c dec -10 if a >= 1",
+            "c inc -20 if c == 10"
+        };
+
+        [Fact]
+        public void Part1_KnownInput_ReturnsLargestRegisterAtTheEnd()
+        {
+            (int actual, int _) = new Day08().Solve(Input);
+
+            Assert.Equal(1, actual);
+        }
+
+        [Fact]
+        public void Part1_InputFile_ReturnsLargestRegisterAtTheEnd()
+        {
+            (int actual, int _) = new Day08().Solve();
+
+            Assert.Equal(5075, actual);
+        }
+
+        [Fact]
+        public void Part2_KnownInput_ReturnsLargestRegisterAtAnyPoint()
+        {
+            (int _, int actual) = new Day08().Solve(Input);
+
+            Assert.Equal(10, actual);
+        }
+
+        [Fact]
+        public void Part2_InputFile_ReturnsLargestRegisterAtAnyPoint()
+        {
+            (int _, int actual) = new Day08().Solve();
+
+            Assert.Equal(7310, actual);
+        }
+    }
+}
