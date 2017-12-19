13cecb40ab823f76835d11e2919969ec4a00cfae
Adam Rodger <adam.rodger@gmail.com>
2017-12-19 08:07:02 +0000

    Day 19

---
+++ src/AdventOfCode/Day19.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for day 19
+    /// </summary>
+    public class Day19
+    {
+        private const char Vertical = '|';
+        private const char Horizontal = '-';
+        private const char Corner = '+';
+        private const char Empty = ' ';
+        private static readonly char[] SpecialCharacters = { Vertical, Horizontal, Corner };
+
+        private enum Direction
+        {
+            Down,
+            Up,
+            Left,
+            Right
+        };
+
+        /// <summary>
+        /// Follow the path set out in the path from the input file and 'collect' letters
+        /// along the way
+        /// </summary>
+        /// <returns>The resultant string of all collected letters and the total number of steps</returns>
+        public (string, int) Solve()
+        {
+            string[] lines = File.ReadAllLines("inputs/day19.txt");
+            return this.Solve(lines);
+        }
+
+        /// <summary>
+        /// Follow the path set out in the path from the given input and 'collect' letters
+        /// along the way
+        /// </summary>
+        /// <param name="lines">Lines drawing the path</param>
+        /// <returns>The resultant string of all collected letters and the total number of steps</returns>
+        public (string, int) Solve(string[] lines)
+        {
+            int y = 0;
+            int x = lines[y].IndexOf(Vertical);
+            int steps = 0;
+            
+            var direction = Direction.Down;
+            var output = new List<char>();
+
+            while (true)
+            {
+                steps++;
+
+                (x, y) = Move(direction, x, y);
+                if (y < 0 || y >= lines.Length || x < 0 || x >= lines[y].Length)
+                {
+                    break;
+                }
+
+                char current = lines[y][x];
+                if (current == Empty)
+                {
+                    break;
+                }
+
+                if (!SpecialCharacters.Contains(current))
+                {
+                    output.Add(current);
+                }
+
+                direction = CheckDirection(lines, direction, x, y);
+            }
+
+            return (new string(output.ToArray()), steps);
+        }
+
+        /// <summary>
+        /// Move in the current direction from the current co-ordinates
+        /// </summary>
+        /// <param name="direction">Direction to move</param>
+        /// <param name="x">Current X co-ordinate</param>
+        /// <param name="y">Current Y co-ordinate</param>
+        /// <returns>New co-ordinates</returns>
+        private static (int x, int y) Move(Direction direction, int x, int y)
+        {
+            switch (direction)
+            {
+                case Direction.Down:
+                    y++;
+                    break;
+                case Direction.Up:
+                    y--;
+                    break;
+                case Direction.Right:
+                    x++;
+                    break;
+                case Direction.Left:
+                    x--;
+                    break;
+            }
+
+            return (x, y);
+        }
+
+        /// <summary>
+        /// Check if the current direction needs to change because a corner is found
+        /// </summary>
+        /// <param name="lines">Puzzle lines</param>
+        /// <param name="current">Current direction</param>
+        /// <param name="x">Current X co-ordinate</param>
+        /// <param name="y">Current Y co-ordinate</param>
+        /// <returns>New direction to travel (which will be the same as current unless a corner was found)</returns>
+        private static Direction CheckDirection(string[] lines, Direction current, int x, int y)
+        {
+            if (lines[y][x] != Corner)
+            {
+                return current;
+            }
+
+            if (current == Direction.Down || current == Direction.Up)
+            {
+                return lines[y][x - 1] == Empty ? Direction.Right : Direction.Left;
+            }
+
+            return lines[y - 1][x] == Empty ? Direction.Down : Direction.Up;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day19Tests.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day19Tests
+    {
+        private static readonly string[] SampleInput =
+        {
+            "    |         ",
+            "    |  +--+   ",
+            "    A  |  C   ",
+            "F---|----E|--+",
+            "    |  |  |  D",
+            "    +B-+  +--+"
+        };
+
+        [Fact]
+        public void Part1_SampleInput()
+        {
+            (string actual, int _) = new Day19().Solve(SampleInput);
+
+            Assert.Equal("ABCDEF", actual);
+        }
+
+        [Fact]
+        public void Part1_RealInput()
+        {
+            (string actual, int _) = new Day19().Solve();
+
+            Assert.Equal("GPALMJSOY", actual);
+        }
+
+        [Fact]
+        public void Part2_SampleInput()
+        {
+            (string _, int actual) = new Day19().Solve(SampleInput);
+
+            Assert.Equal(38, actual);
+        }
+
+        [Fact]
+        public void Part2_RealInput()
+        {
+            (string _, int actual) = new Day19().Solve();
+
+            Assert.Equal(16204, actual);
+        }
+    }
+}
